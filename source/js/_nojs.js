f3180289c305dc21d16a88d132747b002b092b74
Viseko <viseko23@gmail.com>
2021-03-05 06:26:02 +0700

    Исправление доступности меню при отключенном JS

---
+++ source/js/_nojs.js
@@ -0,0 +1,5 @@
+const nojs = document.querySelectorAll('.nojs' );
+
+for (let i = 0; i < nojs.length; i++) {
+  nojs[i].classList.remove('nojs');
+}
--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
 @@include("_menu.js");
 @@include("_slider.js");
 @@include("_modals.js");
+@@include("_nojs.js");
